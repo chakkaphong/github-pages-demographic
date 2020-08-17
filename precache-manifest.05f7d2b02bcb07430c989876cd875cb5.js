fff2afee4e2429c96ed5f355fcf976532fe2668c
chakkaphong <49684818+chakkaphong@users.noreply.github.com>
2020-08-18 00:13:22 +0700

    Updates

---
+++ precache-manifest.05f7d2b02bcb07430c989876cd875cb5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ef62b90fa5d057461cd588ce927ce1",
+    "url": "/github-pages-demographic/index.html"
+  },
+  {
+    "revision": "d27514cde691b0405b8e",
+    "url": "/github-pages-demographic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f36ff564bff3e9e87e12",
+    "url": "/github-pages-demographic/static/js/2.cdee2fc6.chunk.js"
+  },
+  {
+    "revision": "2dfeae871ed0df81b1a1830be7553a53",
+    "url": "/github-pages-demographic/static/js/2.cdee2fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27514cde691b0405b8e",
+    "url": "/github-pages-demographic/static/js/main.4f06753f.chunk.js"
+  },
+  {
+    "revision": "caae674dc5b3c4229ecb",
+    "url": "/github-pages-demographic/static/js/runtime-main.eb6f7032.js"
+  },
+  {
+    "revision": "b0c0d4a13095b319629ff5eee28a5448",
+    "url": "/github-pages-demographic/static/media/babies.b0c0d4a1.jpg"
+  },
+  {
+    "revision": "acfdf77d008af4227551f343a8bda1d2",
+    "url": "/github-pages-demographic/static/media/co2.acfdf77d.jpg"
+  },
+  {
+    "revision": "52791f2739dcdc0facb634d9dd937abb",
+    "url": "/github-pages-demographic/static/media/density.52791f27.jpg"
+  },
+  {
+    "revision": "2e60d0ee1173570531761dfd508eb20a",
+    "url": "/github-pages-demographic/static/media/energy_sector.2e60d0ee.jpg"
+  },
+  {
+    "revision": "c19a5b2f4b6a2ed84888974e70a618ff",
+    "url": "/github-pages-demographic/static/media/grand.c19a5b2f.jpg"
+  },
+  {
+    "revision": "3df65bd92a738c6d26d6d5c6155d637b",
+    "url": "/github-pages-demographic/static/media/pm2.3df65bd9.jpg"
+  },
+  {
+    "revision": "07bab21da36c38f3aff8e7370e363619",
+    "url": "/github-pages-demographic/static/media/urban-rural.07bab21d.jpg"
+  }
+]);
